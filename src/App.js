57dfdc6898fa58c5f4a1762310a7245745f97769
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-07-29 14:04:36 +0530

    this is for generation parser

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CoverLetterGenerator from "./CoverLetterGen";
+import "./App.css";
+
+function App() {
+	return (
+		<div
+			style={{
+				background:
+					"linear-gradient(to right, rgb(255 255 255), rgb(20 37 67))",
+			}}
+		>
+			<CoverLetterGenerator />
+		</div>
+	);
+}
+
+export default App;
