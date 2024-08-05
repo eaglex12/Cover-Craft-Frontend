7d2271208165911736e1e8d7db24547ad2c0e48c
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-08-05 23:05:22 +0530

    integerating acerinity UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import Link from "next/link";
+import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
 
 const Home: React.FC = () => {
 	return (
-		<div>
-			<Link href="/pre-uploaded-letter">
-				Welcome to the Cover Letter Generator
-			</Link>
-			<Link href="/cover-letter-generator">Go to Cover Letter Generator</Link>
+		<div className="flex items-center justify-center  gap-6 p-6">
+			<CardContainer className="p-6">
+				<CardBody className="bg-gray-50 border border-black/[0.1] rounded-xl p-6 shadow-lg">
+					<CardItem
+						translateZ="70"
+						className="text-xl font-bold text-neutral-600 dark:text-white"
+					>
+						Pre-Uploaded Letter
+					</CardItem>
+					<CardItem
+						translateZ="80"
+						className="text-neutral-500 text-sm mt-2 dark:text-neutral-300"
+					>
+						<Link href="/pre-uploaded-letter">
+							<span className="underline cursor-pointer">
+								Go to Pre-Uploaded Letter
+							</span>
+						</Link>
+					</CardItem>
+				</CardBody>
+			</CardContainer>
+
+			<CardContainer className="p-6">
+				<CardBody className="bg-gray-50 border border-black/[0.1] rounded-xl p-6 shadow-lg">
+					<CardItem
+						translateZ="70"
+						className="text-xl font-bold text-neutral-600 dark:text-white"
+					>
+						Cover Letter Generator
+					</CardItem>
+					<CardItem
+						translateZ="80"
+						className="text-neutral-500 text-sm mt-2 dark:text-neutral-300"
+					>
+						<Link href="/cover-letter-generator">
+							<span className="underline cursor-pointer">
+								Go to Cover Letter Generator
+							</span>
+						</Link>
+					</CardItem>
+				</CardBody>
+			</CardContainer>
 		</div>
 	);
 };
