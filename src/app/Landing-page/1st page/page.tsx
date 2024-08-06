77e4b5a80ebcac2de3ec712050092bea0cdfb80b
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-08-06 16:51:45 +0530

    changes in UI of landing page

--- src/app/Landing-page/1st page/page.tsx
+++ src/app/Landing-page/1st page/page.tsx
@@ -5,27 +5,27 @@ import { TypewriterEffectSmooth } from "../../../components/ui/typewriter-effect
 const words = [
 	{
 		text: "Crafting",
-		className: "text-white",
+		className: "text-white text-2xl md:text-4xl",
 	},
 	{
 		text: "Perfect",
-		className: "text-white",
+		className: "text-white text-2xl md:text-4xl",
 	},
 	{
 		text: "Cover",
-		className: "text-white",
+		className: "text-white text-2xl md:text-4xl",
 	},
 	{
 		text: "for",
-		className: "text-white",
+		className: "text-white text-2xl md:text-4xl",
 	},
 	{
 		text: "your",
-		className: "text-white",
+		className: "text-white text-2xl md:text-4xl",
 	},
 	{
 		text: "Success.",
-		className: "text-blue-500 dark:text-blue-500",
+		className: "text-blue-500 text-2xl md:text-4xl dark:text-blue-500",
 	},
 ];
 
@@ -37,9 +37,9 @@ export function FirstPage() {
 				backgroundColor="black"
 				className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
 			>
-				<h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+				<h1 className="text-white text-2xl md:text-6xl font-bold text-center">
 					Cover Craft
-				</h2>
+				</h1>
 				<TypewriterEffectSmooth words={words} />
 				<div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
 					<button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
--- src/app/Landing-page/landingPage.tsx
+++ src/app/Landing-page/landingPage.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
 import { FirstPage } from "./1st page/page";
+import { SecondPage } from "./2nd page/page";
 
 export function About() {
 	return (
 		<>
 			<FirstPage />
+			<SecondPage />
 		</>
 	);
 }
